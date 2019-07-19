3e2c51df9752ca41e995056ea3d221615a5c0179
ohsabry <osabry@touchbistro.com>
2019-07-19 11:22:29 -0400

    Brew formula update for tb version v0.0.10

--- Formula/tb.rb
+++ Formula/tb.rb
@@ -3,11 +3,11 @@ require_relative "../custom_download_strategy"
 class Tb < Formula
   desc "a CLI for running TouchBistro services on a development machine"
   homepage ""
-  version "0.0.9"
+  version "0.0.10"
 
   if OS.mac?
-    url "https://github.com/TouchBistro/tb/releases/download/v0.0.9/tb_0.0.9_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "2e7ca74f471b57a001b5302197f6cc3ec80ee3a3873c57ed68c91eadec2775f3"
+    url "https://github.com/TouchBistro/tb/releases/download/v0.0.10/tb_0.0.10_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "7da1e28205f083b83b73abb03918495e5acdf149b924106ada32b533f0fc0487"
   elsif OS.linux?
   end
 
@@ -16,7 +16,7 @@ class Tb < Formula
     bash_completion.install "dist/tb.bash"
     zsh_completion.install "dist/_tb"
     man1.install Dir["dist/man1/*.1"]
-    system "#{bin}/tb nuke --config"
+    system "make rm-files"
   end
 
   test do
