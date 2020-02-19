1f97292a51a5edb6d700c894d7f23514b34bf915
touchbistro-github <devops@touchbistro.com>
2020-02-19 16:24:11 +0000

    Brew formula update for tb version 2.3.3

--- Formula/tb.rb
+++ Formula/tb.rb
@@ -3,12 +3,12 @@ require_relative "../custom_download_strategy"
 class Tb < Formula
   desc "a CLI for running TouchBistro services on a development machine"
   homepage ""
-  version "2.3.1"
+  version "2.3.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/TouchBistro/tb/releases/download/2.3.1/tb_2.3.1_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "7a41752c1598078e887b5fedda3bc4c87a2b0211674a0655fc3810d5db521220"
+    url "https://github.com/TouchBistro/tb/releases/download/2.3.3/tb_2.3.3_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "f9b02a4b63defe133d59f9bee10c523b34a2d9aa9fbd9d720659b31c10e3ad9a"
   elsif OS.linux?
   end
 
