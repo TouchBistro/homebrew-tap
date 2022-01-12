ac7818ad062d1d64bef46f35d4f5bbde99d66216
touchbistro-github <devops@touchbistro.com>
2022-01-12 01:54:23 +0000

    Brew formula update for tb version 3.6.0

--- Formula/tb.rb
+++ Formula/tb.rb
@@ -5,36 +5,52 @@
 class Tb < Formula
   desc "a CLI for running services on a development machine"
   homepage ""
-  version "3.5.3"
-  bottle :unneeded
+  version "3.6.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/TouchBistro/tb/releases/download/3.5.3/tb_3.5.3_Darwin_x86_64.tar.gz"
-      sha256 "206c8a1c2deffe9227f539710c56be74026e4432e85f65c1df7590ade0bafe13"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/TouchBistro/tb/releases/download/3.5.3/tb_3.5.3_Darwin_arm64.tar.gz"
-      sha256 "7c47ce4009dcf5249e730f703c563a6aead265991c1c7a6c328558cb8af03501"
+      url "https://github.com/TouchBistro/tb/releases/download/3.6.0/tb_3.6.0_Darwin_arm64.tar.gz"
+      sha256 "0e45c392f03891fe5dccb3ffee2efcaefc2c327a88ec05253a958463d9842b83"
+
+      def install
+        bin.install "tb"
+        bash_completion.install "artifacts/tb.bash"
+        zsh_completion.install "artifacts/_tb"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/TouchBistro/tb/releases/download/3.6.0/tb_3.6.0_Darwin_x86_64.tar.gz"
+      sha256 "aeb3700aade33276c5f29aaf4829ee0c46efbcf79eef9fd968fbc913ac837235"
+
+      def install
+        bin.install "tb"
+        bash_completion.install "artifacts/tb.bash"
+        zsh_completion.install "artifacts/_tb"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/TouchBistro/tb/releases/download/3.5.3/tb_3.5.3_Linux_x86_64.tar.gz"
-      sha256 "d8bcac993a0addaafcb3db578a3484bbb5e6bc6823e02be67b6e56efb792b605"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TouchBistro/tb/releases/download/3.5.3/tb_3.5.3_Linux_arm64.tar.gz"
-      sha256 "0778896921c03a97f99b5a54f05ab1ce7ccdce20ffb21af52f2ef13e4431775e"
-    end
-  end
+      url "https://github.com/TouchBistro/tb/releases/download/3.6.0/tb_3.6.0_Linux_arm64.tar.gz"
+      sha256 "0b192b5cea4c71dc5a199d1382658ddb62ca07d48e383f603f9bcd8b327d0ac0"
 
-  def install
-    bin.install "tb"
-    bash_completion.install "artifacts/tb.bash"
-    zsh_completion.install "artifacts/_tb"
-    man1.install Dir["artifacts/man1/*.1"]
+      def install
+        bin.install "tb"
+        bash_completion.install "artifacts/tb.bash"
+        zsh_completion.install "artifacts/_tb"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/TouchBistro/tb/releases/download/3.6.0/tb_3.6.0_Linux_x86_64.tar.gz"
+      sha256 "7f89b0326a6bcb90a8afb17c68aa92c5a86b3e85b2c98969662713a4a333e71d"
+
+      def install
+        bin.install "tb"
+        bash_completion.install "artifacts/tb.bash"
+        zsh_completion.install "artifacts/_tb"
+      end
+    end
   end
 
   test do
